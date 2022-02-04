f4f338cc7647c65518c007ee49071887e845f735
qjh <junhui.qian@einyun.ltd>
2022-02-04 14:27:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Alan
+ * @date 2022/2/4 14:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
